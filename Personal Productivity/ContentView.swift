67351470b4a64758ba2a589e684234d8fbb4ad09
Karlo <dekanic.karlo@gmail.com>
2025-12-31 13:48:27 +0100

    Initial Commit

---
+++ Personal Productivity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Personal Productivity
+//
+//  Created by Karlo Dekanić on 31.12.2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Personal Productivity/Personal_ProductivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_ProductivityApp.swift
+//  Personal Productivity
+//
+//  Created by Karlo Dekanić on 31.12.2025..
+//
+
+import SwiftUI
+
+@main
+struct Personal_ProductivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
